75dd82e610526322d736b1b80f8b8d109b7e02c4
mutefish0 <648262030@qq.com>
2025-03-19 23:59:24 +0800

    simplify

--- src/nodes/core/TempNode.js
+++ src/nodes/core/TempNode.js
@@ -1,62 +1,50 @@
-import Node from './Node.js';
+import Node from "./Node.js";
 
 class TempNode extends Node {
-
 	static get type() {
-
-		return 'TempNode';
-
+		return "TempNode";
 	}
 
-	constructor( type ) {
-
-		super( type );
+	constructor(type) {
+		super(type);
 
 		this.isTempNode = true;
-
 	}
 
-	hasDependencies( builder ) {
-
-		return builder.getDataFromNode( this ).usageCount > 1;
-
+	hasDependencies(builder) {
+		return builder.getDataFromNode(this).usageCount > 1;
 	}
 
-	build( builder, output ) {
-
+	build(builder, output) {
 		const buildStage = builder.getBuildStage();
 
-		if ( buildStage === 'generate' ) {
+		if (buildStage === "generate") {
+			const type = builder.getVectorType(this.getNodeType(builder, output));
+			const nodeData = builder.getDataFromNode(this);
 
-			const type = builder.getVectorType( this.getNodeType( builder, output ) );
-			const nodeData = builder.getDataFromNode( this );
+			if (nodeData.propertyName !== undefined) {
+				return builder.format(nodeData.propertyName, type, output);
+			} else if (
+				type !== "void" &&
+				output !== "void" &&
+				nodeData.usageCount > 1
+			) {
+				const snippet = super.build(builder, type);
 
-			if ( nodeData.propertyName !== undefined ) {
+				const nodeVar = builder.getVarFromNode(this, null, type);
+				const propertyName = builder.getPropertyName(nodeVar);
 
-				return builder.format( nodeData.propertyName, type, output );
-
-			} else if ( type !== 'void' && output !== 'void' && this.hasDependencies( builder ) ) {
-
-				const snippet = super.build( builder, type );
-
-				const nodeVar = builder.getVarFromNode( this, null, type );
-				const propertyName = builder.getPropertyName( nodeVar );
-
-				builder.addLineFlowCode( `${propertyName} = ${snippet}`, this );
+				builder.addLineFlowCode(`${propertyName} = ${snippet}`, this);
 
 				nodeData.snippet = snippet;
 				nodeData.propertyName = propertyName;
 
-				return builder.format( nodeData.propertyName, type, output );
-
+				return builder.format(nodeData.propertyName, type, output);
 			}
-
 		}
 
-		return super.build( builder, output );
-
+		return super.build(builder, output);
 	}
-
 }
 
 export default TempNode;
